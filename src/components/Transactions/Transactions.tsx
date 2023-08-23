e2f7db38369b1d660218e83d5b6c194bc9867106
Lucas Rocha <lucas.voriarocha@gmail.com>
2023-08-23 14:54:01 -0300

    Add react modal

--- src/components/Transactions/Transactions.tsx
+++ src/components/Transactions/Transactions.tsx
@@ -4,14 +4,24 @@ import { useTransactions } from '../../hooks/useTransactions';
 
 import { Link } from 'react-router-dom'
 
+import Modal from "react-modal"
+
 const Transactions: React.FC =  () => {
-    const { transactions, createTransaction, deleteTransaction } = useTransactions()
+    const { transactions, createTransaction, deleteTransaction, getTransactionByRef } = useTransactions()
 
     const [date, setDate] = React.useState('')
     const [buy, setBuy] = React.useState('')
     const [ticker, setTicker] = React.useState('')
     const [quantity, setQuantity] = React.useState(0)
     const [price, setPrice] = React.useState(0)
+
+    const [editModalIsOpen, settEditModalIsOpen] = React.useState(false)
+
+    const [modalDate, setModalDate] = React.useState('')
+    const [modalBuy, setModalBuy] = React.useState('')
+    const [modalTicker, setModalTicker] = React.useState('')
+    const [modalQuantity, setModalQuantity] = React.useState(0)
+    const [modalPrice, setModalPrice] = React.useState(0)
 
     const handleAddTransaction = async (event: FormEvent) => {
         event.preventDefault()
@@ -35,50 +45,87 @@ const Transactions: React.FC =  () => {
         await deleteTransaction(ref)
     }
 
+    const handleOpenModal = async (ref: number) => {
+        const modalData = await getTransactionByRef(ref)  
+
+        setModalDate(modalData.data.date)
+        setModalBuy(modalData.data.buy)
+        setModalTicker(modalData.data.ticker)
+        setModalQuantity(modalData.data.quantity)
+        setModalPrice(modalData.data.price)
+        
+        settEditModalIsOpen(true)
+    }
+
+    const handleCloseModal = () => {
+        settEditModalIsOpen(false)
+    }
+
+    Modal.setAppElement('#root')
+
     return (
         <>
-        <Link to='/dashboard'>
-            <button type="button" >
-                    Dashboard
+            <Link to='/dashboard'>
+                <button type="button" >
+                        Dashboard
+                </button>
+            </Link>
+
+            <button onClick={() => console.log(transactions)}>
+                console                            
             </button>
-        </Link>
 
-        <button onClick={() => console.log(transactions)}>
-            console                            
-        </button>
+            {transactions.map((transaction) => {
+                return (
+                    <div key={transaction.ref?.value.id}>
+                        <p>{transaction.data.date}</p>
+                        {transaction.data.buy ? <p>Compra</p> : <p>Venda</p>}
+                        <p>{transaction.data.ticker}</p>
+                        <p>{transaction.data.quantity}</p>
+                        <p>{transaction.data.price}</p>
+                        <button onClick={() => handleDeleteTransaction(transaction.ref.value.id)}>
+                            Remove
+                        </button>
+                        <button onClick={() => handleOpenModal(transaction.ref.value.id)}>
+                            Edit
+                        </button>
+                    </div>
+                )
+            })}
 
-        {transactions.map((transaction) => {
-            return (
-                <div key={transaction.ref?.value.id}>
-                    <p>{transaction.data.date}</p>
-                    {transaction.data.buy ? <p>Compra</p> : <p>Venda</p>}
-                    <p>{transaction.data.ticker}</p>
-                    <p>{transaction.data.quantity}</p>
-                    <p>{transaction.data.price}</p>
-                    <button onClick={() => handleDeleteTransaction(transaction.ref?.value.id)}>
-                        Remove
-                    </button>
-                    <button>
-                        Edit
-                    </button>
-                </div>
-            )
-        })}
+            <form onSubmit={handleAddTransaction}>
+                <label htmlFor="">Data</label>
+                <input type="text" value={date} onChange={event => setDate(event.target.value)} />
+                <label htmlFor="">Compra/Venda</label>
+                <input type="text" value={buy} onChange={event => setBuy(event.target.value)} />
+                <label htmlFor="">Ticker</label>
+                <input type="text" value={ticker} onChange={event => setTicker(event.target.value)} />
+                <label htmlFor="">Quantidade</label>
+                <input type="number" value={quantity} onChange={event => setQuantity(Number(event.target.value))} />
+                <label htmlFor="">Preço</label>
+                <input type="number" value={price} onChange={event => setPrice(Number(event.target.value))} />
+                <button type='submit'>Adicionar</button>
+            </form>
 
-        <form onSubmit={handleAddTransaction}>
-            <label htmlFor="">Data</label>
-            <input type="text" value={date} onChange={event => setDate(event.target.value)} />
-            <label htmlFor="">Compra/Venda</label>
-            <input type="text" value={buy} onChange={event => setBuy(event.target.value)} />
-            <label htmlFor="">Ticker</label>
-            <input type="text" value={ticker} onChange={event => setTicker(event.target.value)} />
-            <label htmlFor="">Quantidade</label>
-            <input type="number" value={quantity} onChange={event => setQuantity(Number(event.target.value))} />
-            <label htmlFor="">Preço</label>
-            <input type="number" value={price} onChange={event => setPrice(Number(event.target.value))} />
-            <button type='submit'>Adicionar</button>
-        </form>
-    </>
+            <Modal
+                isOpen={editModalIsOpen}
+                onRequestClose={handleCloseModal}
+            >
+                <form onSubmit={handleAddTransaction}>
+                    <label htmlFor="">Data</label>
+                    <input type="text" value={modalDate} onChange={event => setDate(event.target.value)} />
+                    <label htmlFor="">Compra/Venda</label>
+                    <input type="text" value={modalBuy} onChange={event => setBuy(event.target.value)} />
+                    <label htmlFor="">Ticker</label>
+                    <input type="text" value={modalTicker} onChange={event => setTicker(event.target.value)} />
+                    <label htmlFor="">Quantidade</label>
+                    <input type="number" value={modalQuantity} onChange={event => setQuantity(Number(event.target.value))} />
+                    <label htmlFor="">Preço</label>
+                    <input type="number" value={modalPrice} onChange={event => setPrice(Number(event.target.value))} />
+                    <button type='submit'>Adicionar</button>
+                </form>
+            </Modal>
+        </>
     )
 }
 
--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -39,7 +39,8 @@ interface TransactionProviderProps {
 interface TransactionsContextData {
     transactions: Transaction[],
     createTransaction: (transaction: TransactionInput) => Promise<void>,
-    deleteTransaction: (ref: number) => Promise<void>
+    deleteTransaction: (ref: number) => Promise<void>,
+    getTransactionByRef: (ref: number) => Promise<Transaction>
 }   
 
 export const TransactionContext = createContext<TransactionsContextData>(
@@ -71,9 +72,21 @@ export const TransactionsProvider = ({children}: TransactionProviderProps) => {
     const deleteTransaction = async (ref: number) => {
         await client.query(q.Delete(q.Ref(q.Collection("Transactions"), ref)))
     }
+
+    const getTransactionByRef = async (ref: number): Promise<Transaction> => {
+        return await client.query(q.Get(q.Ref(q.Collection('Transactions'), ref)))
+    }
+
+   // export const getContactByRef = ref => {
+   ///     return client.query(q.Get(q.Ref(q.Collection('contacts'), ref)))
+//      }
+      
+  //    export const updateContact = ref => {
+    //    return client.query(q.Update(q.Ref(q.Collection('contacts'), ref)))
+     // }
     
     return (
-        <TransactionContext.Provider value={{ transactions, createTransaction, deleteTransaction }}>
+        <TransactionContext.Provider value={{ transactions, createTransaction, deleteTransaction, getTransactionByRef }}>
             {children}
         </TransactionContext.Provider>
     )
